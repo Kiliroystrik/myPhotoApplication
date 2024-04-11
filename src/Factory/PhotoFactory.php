473d98ecc9a4e7468de15334153da8a133ab2130
Kiliroy <borges.mathieu@gmail.com>
2024-04-12 00:06:26 +0200

    Fixed lorem ipsum api

--- src/Factory/PhotoFactory.php
+++ src/Factory/PhotoFactory.php
@@ -46,9 +46,24 @@ final class PhotoFactory extends ModelFactory
      */
     protected function getDefaults(): array
     {
-        $imageId = self::faker()->numberBetween(1, 1000); // Choose a random image from Lorem Picsum
-        $imageUrl = "https://picsum.photos/" . $imageId . "/640/480";
+        $page = self::faker()->numberBetween(1, 10); // Choisissez une page aléatoire entre 1 et 10
+        $limit = 100; // Nombre d'images par page
 
+        $imageListUrl = "https://picsum.photos/v2/list?page=$page&limit=$limit";
+        $imageListJson = file_get_contents($imageListUrl);
+        $imageList = json_decode($imageListJson, true);
+
+        $randomImage = self::faker()->randomElement($imageList);
+        $imageId = $randomImage['id'] ?? null;
+        $author = $randomImage['author'] ?? '';
+        $width = $randomImage['width'] ?? 640;
+        $height = $randomImage['height'] ?? 480;
+
+        if (!$imageId) {
+            throw new \Exception("Failed to fetch image details.");
+        }
+
+        $imageUrl = "https://picsum.photos/id/$imageId/$width/$height";
 
         return [
             'createdAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
@@ -56,10 +71,16 @@ final class PhotoFactory extends ModelFactory
             'title' => self::faker()->words(3, true),
             'description' => self::faker()->text(255),
             'imageUrl' => $imageUrl,
-            'metaInfo' => ["info" => self::faker()->text()],
+            'metaInfo' => [
+                'author' => $author,
+                'width' => $width,
+                'height' => $height,
+                'info' => self::faker()->text(),
+            ],
             'tags' => TagFactory::createMany(2),
         ];
     }
+
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
